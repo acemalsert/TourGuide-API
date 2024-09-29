519a64a0be99f913a745871654cd21684f7833f8
AhmetCemalSert <146725922+AhmetCemalSert@users.noreply.github.com>
2024-09-29 18:28:30 +0300

    program.cs updated

--- TourGuide/Program.cs
+++ TourGuide/Program.cs
@@ -3,55 +3,6 @@ using TourGuide.Persistance;
 using TourGuide.Mapper;
 using TourGuide.Application.Exceptions;
 
-//var builder = WebApplication.CreateBuilder(args);
-
-//// Add services to the container.
-
-//builder.Services.AddCors(options =>
-//{
-//    options.AddPolicy("AllowSpecificOrigins",
-//        policy =>
-//        {
-//            policy.WithOrigins("http://localhost:5173") 
-//                  .AllowAnyHeader()
-//                  .AllowAnyMethod();
-//        });
-//});
-
-//builder.Services.AddControllers();
-//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
-
-
-//var env = builder.Environment;
-
-//builder.Configuration
-//    .SetBasePath(env.ContentRootPath)
-//    .AddJsonFile("appsettings.json", optional: false)
-//    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
-
-//builder.Services.AddPersistence(builder.Configuration);
-//builder.Services.AddApplication();
-//builder.Services.AddCustomMapper();
-
-//var app = builder.Build();
-//app.UseCors("AllowSpecificOrigins");
-
-//// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-//    app.UseSwagger();
-//    app.UseSwaggerUI();
-//}
-
-//app.ConfigureExceptionHandlingMiddleware();
-//app.UseAuthorization();
-
-//app.MapControllers();
-
-//app.Run();
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -102,4 +53,4 @@ app.MapControllers();
 
 app.Run();
 
-// DERS 16 da kaldım. Ruleslar doğru calısıyor mu bir test edilmeli
+// JWT - Access ve Refresh Token Servisi Yapılmalı
