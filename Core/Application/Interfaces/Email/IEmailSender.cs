808805c14b3c92f1bbe85bb494d21fc746e7f9d0
AhmetCemalSert <146725922+AhmetCemalSert@users.noreply.github.com>
2024-11-25 14:52:34 +0300

    folder structure updated

---
+++ Core/Application/Interfaces/Email/IEmailSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourGuide.Application.Interfaces.Email
+{
+    public interface IEmailSender
+    {
+        //Task<bool> SendEmail(EmailMessage email);
+    }
+}
